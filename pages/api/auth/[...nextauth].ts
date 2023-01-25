6ba46b9ba9dd4f62256ae723adc2ec7376a58e93
yuki <shimazuyuki7@gmail.com>
2023-01-25 17:30:10 +0900

    chore: change auth from github to 42

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,7 +3,8 @@
 import { NextApiHandler } from "next";
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import GitHubProvider from "next-auth/providers/github";
+// import GitHubProvider from "next-auth/providers/github";
+import FortyTwoProvider from "next-auth/providers/42-school";
 import prisma from "../../../lib/prisma";
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
@@ -11,9 +12,13 @@ export default authHandler;
 
 const options = {
   providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    // GitHubProvider({
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET,
+    // }),
+    FortyTwoProvider({
+      clientId: process.env.FORTY_TWO_CLIENT_ID,
+      clientSecret: process.env.FORTY_TWO_CLIENT_SECRET,
     }),
   ],
   adapter: PrismaAdapter(prisma),
